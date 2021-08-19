b2472545db61a05ea502b216d22ba40360eb5024
Wsan(笑さん) <32955729+wswsans@users.noreply.github.com>
2021-08-19 16:42:12 +0900

    Remaked Frame
    フレームの原型
    説明はReadmeに書く予定

---
+++ frame/webdata.js
@@ -0,0 +1,10 @@
+webdata = {
+	"name": "FrameTest",
+	"urls": [
+		"test1",
+		"test2",
+		"whatisyourfavorite",
+		// "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+	],
+	"default_url": "test1"
+}
